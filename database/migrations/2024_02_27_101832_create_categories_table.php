95193396a82168a8077bee4dc6084065a215af08
Mohamed Atya Sobhy Hawash <117924276+mhmdatya72@users.noreply.github.com>
2024-05-04 22:52:23 +0300

    Update 2024_02_27_101832_create_categories_table.php

--- database/migrations/2024_02_27_101832_create_categories_table.php
+++ database/migrations/2024_02_27_101832_create_categories_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name_arabic')->nullable(); // Arabic column for name
-            $table->string('name_english')->nullable(); // English column for name
-            $table->text('description_arabic')->nullable(); // Arabic column for description
-            $table->text('description_english')->nullable(); // English column for description
+            $table->string('name_ar')->nullable(); // Arabic column for name
+            $table->string('name_en')->nullable(); // English column for name
+            $table->text('description_ar')->nullable(); // Arabic column for description
+            $table->text('description_en')->nullable(); // English column for description
             $table->string('image')->nullable();
             $table->timestamps();
         });
